48402b066395ed7cfeb31e1c25d54eb64bcac711
Giridhar Krishnagiri <giri03@yahoo.com>
2026-03-21 13:23:30 +0000

    Phase 2.4: Added first version of Orchestrator

---
+++ backend/agents/orchestrator.py
@@ -0,0 +1,27 @@
+from services.intent_classifier import classify_intent
+from rag.retriever import search
+
+
+def orchestrate(query: str):
+    """
+    Main brain of the system
+    """
+
+    intent = classify_intent(query)
+
+    print(f"[Orchestrator] Intent: {intent}")
+
+    if intent == "RAG":
+        return search(query)
+
+    elif intent == "ACTION":
+        return {
+            "message": "UI actions not implemented yet",
+            "intent": intent
+        }
+
+    else:
+        return {
+            "message": "Could not determine intent",
+            "intent": intent
+        }
